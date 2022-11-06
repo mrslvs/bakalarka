baad19ed62db00bdd0218990a8d9a9f7239b05e7
mrslvs <sekerka.miroslav@outlook.com>
2022-11-06 15:01:46 +0100

    refactor folder structure

--- electron/electron.js
+++ electron/electron.js
@@ -9,8 +9,8 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       nodeIntegration: true,
     },
