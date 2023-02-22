1d688f1735f018e2c34d6c452ddc913a59b5bf7e
mrslvs <sekerka.miroslav@outlook.com>
2023-02-22 21:21:29 +0100

    fix communication

--- src/API/serial.js
+++ src/API/serial.js
@@ -13,25 +13,45 @@ const parser = new ReadlineParser();
 
 port.pipe(parser);
 
+// const startCom = async (event) => {
+//     // zmenit funkcu startCom ab
+//     parser.on('data', (data) => {
+//         if (data == 1111) {
+//             // console.log('comm initiated');
+//             // send joystick input
+//             port.write('11lx0.20\n'); // rovnovazny stav 0,0
+//         } else if (data == 2222) {
+//             return;
+//         } else {
+//             // funkcia2(data);
+//             // funkcia3(event, data);
+//             console.log(data);
+//             event.reply('startComResponse', data);
+//             return data;
+//         }
+//     });
+// };
+
 const startCom = async (event) => {
-    parser.on('data', (data) => {
-        if (data == 1111) {
-            // console.log('comm initiated');
-            // send joystick input
-            port.write('0.20\n'); // rovnovazny stav 0,0
-        } else {
-            // funkcia2(data);
-            funkcia3(event, data);
-            return data;
-        }
-    });
+    // zmenit funkcu startCom ab
+
+    port.write('1111\n');
 };
 
-const funkcia3 = (event, sprava) => {
-    // console.log('funkcia3:');
-    // console.log(sprava);
-    event.reply('startComResponse', sprava);
-};
+parser.on('data', (data) => {
+    if (data == 1111) {
+        // console.log('comm initiated');
+        // send joystick input
+        // port.write('11lx0.20\n'); // rovnovazny stav 0,0
+        console.log('received 1111');
+    }
+});
+
+// const funkcia3 = (event, sprava) => {
+//     // console.log('funkcia3:');
+//     // console.log(sprava);
+//     event.reply('startComResponse', sprava);
+// };
 
 const getAvailablePorts = async () => {
     const ports = await SerialPort.list();
