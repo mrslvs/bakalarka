483f603681aff2837ee9099a38835af0316eb547
mrslvs <sekerka.miroslav@outlook.com>
2023-02-25 18:11:16 +0100

    add joystick

--- sketch_feb25.ino
+++ sketch_feb25.ino
@@ -1,8 +1,6 @@
 #include <Servo.h>
 #include <NewPing.h>
 
-#define START_COMM 1111
-#define END_COMM 2222
 #define PIN_TRIGGER 12
 #define PIN_ECHO 13
 #define MAX_DISTANCE 450
@@ -20,10 +18,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-//  Serial.println(START_COMM); // initiate comm
-//  delay(300);
-
+  
 // SONAR
   int distance = sonar.ping_cm();
   delay(5);
@@ -41,76 +36,26 @@ void loop() {
 
   String output_message = String(distance) + ',' + String(servo_global);
 
-  char receivedByte;
-  char message[50];
-  int pos = 0;
-
-
   if(Serial.available() > 0){
     processMessage();
   }
-
-
-//  if(Serial.available() > 0){
-//    while(Serial.available()>0){
-//      receivedByte = Serial.read();
-//      
-//      if(receivedByte == '\n'){
-//        // read the line, now process
-//        message[pos] = '\0';
-//        String msg = String(message);
-////        if(message[0] == '1' && message[1] == '1' && message[2] == '1' && message[3] == '1'){
-//        if(isStart(msg)){
-//          Serial.println("1111");
-////          Serial.println(message);
-//        }else{
-//          Serial.println("5555");
-//        }
-//      }else{
-//        message[pos] = receivedByte;
-//      }
-//      pos++;
-//    }
-//  }
-
-
-
-
-
-
-
-  
-//  while(Serial.available() > 0){
-//    char receivedByte = Serial.read();
-//    if(receivedByte == '\n'){
-//      message[pos] = '\0';
-//      if(message[0]=='1' && message[1]=='1'){
-//        servo_global += 5;
-////        Serial.println(output_message);
-//        Serial.println("test1");
-//      }
-//    }else{
-//      message[pos] = receivedByte;
-//    }
-//    pos++;
-//  }
-
-  // end comm
-//  Serial.println(END_COMM);
-
 }
 
 void processMessage(){
   String startString = "startpass";
   String str = Serial.readStringUntil('\n');
 
-  Serial.println(str.substring(startString.length()));
+  if(str.startsWith(startString)){
+//    Serial.println(str.substring(startString.length()));
+    float tmp = getJoystick(str.substring(startString.length()));
+    Serial.println(String(tmp));
+  }
 }
 
-boolean isStart(String message){
-  if(message[0] == '1' && message[1] == '1' && message[2] == '1' && message[3] == '1'){
-    return true;
-  }else{
-    return false;
-  }
+float getJoystick(String msg){
+  float joystick;
+  String tmp = msg.substring(2);
+
+  joystick = tmp.toFloat();
+  return joystick;
 }
--- src/API/serial.js
+++ src/API/serial.js
@@ -38,8 +38,9 @@ const startCom = async (event) => {
     setInterval(function () {
         // your code...
         if (count < 5) {
-            port.write('startpassLX0.00ly0.00rx0.00ry0.00\n');
+            // port.write('startpassLX0.00ly0.00rx0.00ry0.00\n');
             // port.write('xy11111xy\n');
+            port.write('startpassLX0.00\n');
         }
         count++;
     }, 300);
