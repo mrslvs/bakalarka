afe11f6f7c8d015eb9ed5c7943cb755ce0367feb
mrslvs <sekerka.miroslav@outlook.com>
2023-04-16 18:21:08 +0200

    fix animation height

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const { ipcRenderer } = require('electron');
 // TODO:
 // delete PortForm.jsx ?
 // separate joystick
+// min-max for arduino
 
 function App() {
     const [availablePorts, setAvailablePorts] = useState('');
